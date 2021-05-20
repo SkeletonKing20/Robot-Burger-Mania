f164e1dfcf59f78672f82a33ead9f153d51decc3
DuhanTozun <duhan96@live.de>
2021-05-20 14:36:56 +0200

    added enemy movement

---
+++ Main Game/Assets/Scripts/Enemy.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    public float speed = 5f;
+    
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        transform.Translate(-Vector2.right * speed * Time.deltaTime);
+    }
+
+}
