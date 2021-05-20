add4f65d8f3a524008045483bfe8d102edc5b5fc
DuhanTozun <duhan96@live.de>
2021-05-20 15:33:29 +0200

    Update Enemy.cs

--- Main Game/Assets/Scripts/Enemy.cs
+++ Main Game/Assets/Scripts/Enemy.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class Enemy : MonoBehaviour
 {
     public float speed = 5f;
-    
+    public float JumpForce = 3;
+    private Rigidbody2D _rigidbody;
+    public float cooldown = 0.5f;
 
     void Start()
     {
-        
+        _rigidbody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
         transform.Translate(-Vector2.right * speed * Time.deltaTime);
+        
+        if(Time.time > cooldown && Mathf.Abs(_rigidbody.velocity.y) < 0.001f)
+        {
+            _rigidbody.AddForce(new Vector2(0, JumpForce), ForceMode2D.Impulse);
+            cooldown = Time.time + 0.5f;
+        }
     }
 
 }
