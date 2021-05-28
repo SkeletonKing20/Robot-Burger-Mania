632aa0253f27384edebb8d73dad4ba0b7a933a09
DuhanTozun <duhan96@live.de>
2021-05-28 16:26:57 +0200

    textbox

---
+++ Main Game/Assets/Scripts/UItext.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class UItext : MonoBehaviour
+{
+    [SerializeField] private TMP_Text textLabel;
+
+    private void Start()
+    {
+        GetComponent<AutoText>().Run("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA",textLabel );
+    }
+
+}
