9cfd4d24f3a5eb7c5355bb889d80f152d0fb745e
SkeletonKing20 <n.bonn2002@gmail.com>
2021-06-02 15:50:03 +0200

    chickenBurger

--- Main Game/Assets/Scripts/chickenWingBurger.cs
+++ Main Game/Assets/Scripts/chickenWingBurger.cs
@@ -12,24 +12,28 @@ public class chickenWingBurger : Enemy
 
     float diveStep;
 
-    bool isDiving;
+    public bool isDiving;
     public bool isSitting;
 
+    float initialHeight;
     public override void Start()
     {
         base.Start();
         diveStep = speed * Time.deltaTime;
+        currentPosition = transform.position;
+        cooldown = 5f;
+        initialHeight = currentPosition.y;
     }
 
     private void Update()
     {
         if (!isSitting)
         {
-            if (Time.time > currentAttackCoolDown && !isDiving)
+            if(Time.time > cooldown)
             {
                 isDiving = true;
                 targetPosition = player.transform.position;
-                currentAttackCoolDown = Time.time + attackCoolDown;
+                cooldown = Time.time + 5f;
             }
             if(isDiving)
             {
@@ -39,9 +43,20 @@ public class chickenWingBurger : Enemy
         }
     }
 
+    public void returnToHeight(float height)
+    {
+        Vector3 target = new Vector3(transform.position.x, height, 0);
+        while(transform.position.y < height)
+        Vector3.MoveTowards(transform.position,target, diveStep);
+    }
     public void Dive()
     {
         diveStep = speed * Time.deltaTime;
         transform.position = Vector3.MoveTowards(currentPosition, targetPosition, diveStep);
+        if(Mathf.Abs(transform.position.x - targetPosition.x) < 0.000001f)
+        {
+            isDiving = false;
+            returnToHeight(initialHeight);
+        }
     }
 }
