2dab1535bdc1c97c05ebe228bb00417b72d35f46
DuhanTozun <duhan96@live.de>
2021-06-02 16:14:50 +0200

    updated projectile script

--- Main Game/Assets/Scripts/ShootProjectile.cs
+++ Main Game/Assets/Scripts/ShootProjectile.cs
@@ -6,15 +6,20 @@ public class ShootProjectile : MonoBehaviour
 {
     public float speed = 10;
     private Rigidbody2D rb;
+
     void Start()
     {      
-
         rb = this.GetComponent<Rigidbody2D>();
         rb.velocity = new Vector2(-speed, 0);
+        StartCoroutine(SelfDestruct());
+    }
+    IEnumerator SelfDestruct()
+    {
+        yield return new WaitForSeconds(20f);
+        Destroy(this.gameObject);
     }
 
-
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(this.gameObject);
     }
