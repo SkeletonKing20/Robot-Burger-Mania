27ed08c72f71d523a594c4f365e42e4726183ee6
SkeletonKing20 <n.bonn2002@gmail.com>
2021-06-04 12:30:42 +0200

    scrapped and redone enemy ai

---
+++ Main Game/Assets/Scripts/ScriptsForNewEnemy/ChickenWings.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChickenWings : NewEnemyScript
+{
+    public Vector3[] PatrolPoints;
+    public override IEnumerator IDLE()
+    {
+        animator.transform.localScale = PatrolPoints[1].x > transform.position.x ? scaleRight : scaleLeft;
+        while(states == ENEMY_STATE.IDLE && transform.position != PatrolPoints[1])
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PatrolPoints[1], deltaStep);
+            yield return null;
+        }
+        yield return new WaitForSeconds(5f);
+        animator.transform.localScale = PatrolPoints[1].x > transform.position.x ? scaleRight : scaleLeft;
+        while (states == ENEMY_STATE.IDLE && transform.position != PatrolPoints[0])
+        {
+            transform.position = Vector3.MoveTowards(transform.position, PatrolPoints[0], deltaStep);
+            yield return null;
+        }
+        yield return new WaitForSeconds(5f);
+    }
+
+    public override IEnumerator CHASE()
+    {
+        lastPlayerPosition = player.transform.position;
+        while (transform.position != lastPlayerPosition)
+        {
+            transform.position = Vector3.MoveTowards(transform.position, lastPlayerPosition, deltaStep * 2);
+            yield return null;
+        }
+        states = ENEMY_STATE.IDLE;
+    }
+
+    public override IEnumerator DAMAGE()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerator DIE()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public override IEnumerator ATTACK()
+    {
+        throw new System.NotImplementedException();
+    }
+}
---
+++ Main Game/Assets/Scripts/ScriptsForNewEnemy/NewEnemyScript.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class NewEnemyScript : MonoBehaviour
+{
+    public ENEMY_STATE states;
+    public float moveSpeed;
+    public float deltaStep;
+
+    protected Vector3 scaleLeft = new Vector3(1,1,1);
+    protected Vector3 scaleRight = new Vector3(-1, 1, 1);
+
+    protected Vector3 lastPlayerPosition;
+
+    public Animator animator;
+    public SpriteRenderer spriteR;
+    public Player player;
+    private void Awake()
+    {
+        states = ENEMY_STATE.IDLE;
+    }
+
+    private void Start()
+    {
+        deltaStep = moveSpeed * Time.deltaTime;
+        animator = GetComponentInChildren<Animator>();
+        spriteR = GetComponentInChildren<SpriteRenderer>();
+        player = FindObjectOfType<Player>();
+        lastPlayerPosition = player.transform.position;
+        StartCoroutine(EnemyFSM());
+    }
+
+    IEnumerator EnemyFSM()
+    {
+        while(true)
+        {
+            yield return StartCoroutine(states.ToString());
+        }
+    }
+    public enum ENEMY_STATE
+    {
+        IDLE = 0,
+        CHASE = 1,
+        ATTACK = 2,
+    }
+
+    public abstract IEnumerator IDLE();
+    public abstract IEnumerator ATTACK();
+    public abstract IEnumerator CHASE();
+    public abstract IEnumerator DAMAGE();
+    public abstract IEnumerator DIE();
+}
