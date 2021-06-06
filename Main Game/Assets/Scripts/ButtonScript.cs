f7f10876bbbd54f9b383574991132292c971d0de
SkeletonKing20 <n.bonn2002@gmail.com>
2021-06-06 13:24:49 +0200

    menues

---
+++ Main Game/Assets/Scripts/ButtonScript.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonScript : MonoBehaviour
+{
+    SpriteRenderer spriteR;
+    [SerializeField]
+    Sprite hoverSprite;
+    Sprite initialSprite;
+    private void Start()
+    {
+        spriteR = GetComponent<SpriteRenderer>();
+        initialSprite = spriteR.sprite;
+    }
+    public void Restart()
+        {
+
+        }
+        public void MainMenu()
+        {
+
+        }
+
+        public void Quit()
+        {
+
+        }
+
+        public void StartGame()
+        {
+
+        }
+
+    private void OnMouseEnter()
+    {
+        spriteR.sprite = hoverSprite;
+    }
+
+    private void OnMouseExit()
+    {
+        spriteR.sprite = initialSprite;
+    }
+}
